ed228e595d11aeff81c2cab27972a1a2e0add0fe
Kevin Barnes <kbarnes3@gmail.com>
2023-05-25 10:34:38 -0700

    Fixing linting

--- src/app/version-info.ts
+++ src/app/version-info.ts
@@ -1,6 +1,5 @@
 export const versionInfo = (() => {
   try {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
     return require('../../git-version.json');
   } catch {
     // In dev the file might not exist:

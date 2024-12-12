52b962b61d4def5fb20a2a41661807f6a9c35410
Kevin Barnes <kbarnes3@gmail.com>
2024-12-12 15:04:24 -0800

    Update to @angular/core @ "19.0.4"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { versionInfo } from './version-info';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   title = 'Base Angular App';
